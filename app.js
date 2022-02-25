e91c71313fdfb25a98bf532e979b5a07429720cd
william <williamw8828@gmail.com>
2022-02-25 15:36:56 -0500

    added pseudo code

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // creating a blackjack Game!
+
+// create a deck of cards (array with 52 cards object with suite and number)
+
+// create a new array and randomize it
+
+// create two objects: (one for dealer and one for user)
+    const dealer = {
+        hand: []
+    }
+
+    const player1 = {
+        hand: []
+    }
+
+// create a method that will append that first index of the new randomized array to hand array
+
+// create method that will deal the openinng of the game (append splice card to hand array)
+
+// create a method to add the cards and to display the total for the user
+
+// create a method "hit" that will allow the user to add a card (append splice 1 card to hand array)
+    // call the method to sum the cards
+    // if the sum is over 21 game over appears
+
+// if the user clicks "stay" the dealers cards are shown and compared. Winner is chosen
